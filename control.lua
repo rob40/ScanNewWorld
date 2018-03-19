185f579eaefbf25088ce697486f9002a810c3573
rob40 <rpowers3201@aol.com>
2018-03-19 17:22:49 -0500

    Speed up map charting

--- control.lua
+++ control.lua
@@ -8,7 +8,6 @@ end)
 
 script.on_event(defines.events.on_tick, function(event)
   scan()
-  finished()
 end)
 
 function scan()
@@ -17,24 +16,5 @@ function scan()
   local f = player.force
   local s = player.surface
   local z = Scan_size/2
-  for k = 6,z do
-    if game.tick ==((k^2)*24) - 10*60 then 
-      f.chart(s, {{-32*k, -32*k}, {32*k, 32*k}})
-      if k == z then
-        global.scan_starting_area = nil
-        Scan_size = nil
-        global.print_finished_message = game.tick + 10*60
-      end
-      return
-    end
-  end
-end
-
-
-
-function finished()
-  if not global.print_finished_message then return end
-  if game.tick ~= global.print_finished_message then return end
-  game.print("Initial map scan is finished")
-  global.print_finished_message = nil
+  f.chart(s, {{-Scan_size, -Scan_size}, {Scan_size, Scan_size}})
 end
