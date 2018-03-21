c2e369f804eb87a286ada5af051ab5e95f97bc9b
rob40 <rpowers3201@aol.com>
2018-03-21 18:20:55 -0500

    Fix bug that limited the initial scan size

--- control.lua
+++ control.lua
@@ -1,20 +1,6 @@
 Scan_size = settings.startup["InitialScan-ChunkScan"].value --Scan size in chunks
 
 script.on_event(defines.events.on_player_created, function(event)
-  if event.player_index == 1 then
-    global.scan_starting_area = true
-  end
+  local player = game.players[event.player_index]
+  player.force.chart(player.surface, {{player.position.x - Scan_size, player.position.y - Scan_size}, {player.position.x + Scan_size, player.position.y + Scan_size}})
 end)
-
-script.on_event(defines.events.on_tick, function(event)
-  scan()
-end)
-
-function scan()
-  if not global.scan_starting_area then return end
-  local player = game.players[1]
-  local f = player.force
-  local s = player.surface
-  local z = Scan_size/2
-  f.chart(s, {{-Scan_size, -Scan_size}, {Scan_size, Scan_size}})
-end
